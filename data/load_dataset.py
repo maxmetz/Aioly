f588dca2ee070ade62077bb16f14f24c47029ba3
florent-abdelghafour <124244994+florent-abdelghafour@users.noreply.github.com>
2024-07-01 12:24:57 +0200

    Add files via upload

--- data/load_dataset.py
+++ data/load_dataset.py
@@ -8,45 +8,67 @@ import matplotlib.pyplot as plt
 
 
 class SoilSpectralDataSet(Dataset):
-    def __init__(self, dataset_type="visnir", data_path=None, preprocessing=None):
+    def __init__(self, dataset_type="visnir", data_path=None, preprocessing=None, y_cols=None,y_labels="oc.usda.c729"):
         if data_path==None:
+            # Set default data path in project path if none provided
             rel_dir = os.path.dirname(os.path.abspath(__file__))
             data_path = os.path.join(rel_dir, 'data/datasets/oss/ossl_all_L1_v1.2.csv')
             
         self.data_path = data_path
         self.oss_data_path = data_path
         self.preprocessing =preprocessing
+        self.y_cols = y_cols
       
         
-        
+       # Load data
         try:
-           data_raw = pd.read_csv(self.data_path, low_memory=False)
+            data_raw = pd.read_csv(self.data_path, low_memory=False)
         except FileNotFoundError:
-           raise ValueError(f"File not found: {self.data_path}")
-        Y = np.array(data_raw.filter(regex = "oc.usda.c729"))
-        mask = ~np.isnan(Y)[:,0]
+            raise ValueError(f"File not found: {self.data_path}")
+            
+            
+        # Handle y_labels parameter
+        if isinstance(y_labels, str):
+            y_labels = [y_labels]
+        self.y_labels = y_labels
+        
+        # Extract target variables
+        Y = np.array(data_raw.filter(regex="|".join(y_labels)))
+        mask = ~np.isnan(Y).any(axis=1)
+        
+        # Initialize X and Y based on dataset type
+        if dataset_type == "mir":
+           self.X, self.Y = self._process_data(data_raw, mask, "mir", "abs")
+        elif dataset_type == "nir":
+           self.X, self.Y = self._process_data(data_raw, mask, "visnir", "ref")
+        else:
+           raise ValueError("dataset_type must be either 'mir' or 'nir'")
+       
+        self.spec_dims=self.X.shape[1]
+        # Apply preprocessing if provided
+        if self.preprocessing:
+           self.X = self.preprocessing(self.X)
         
 
-        if dataset_type == "mir":
-            mir = np.array(data_raw.filter(regex = "mir").filter(regex = "abs"))
-            mask_ = ~(np.isnan(mir[:,0])[mask])            
-            self.X = mir[mask][mask_]
-            self.Y = Y[mask][mask_]
-
-        if dataset_type == "nir":
-            nir = np.array(data_raw.filter(regex = "visnir").filter(regex = "ref"))
-            mask_ = ~(np.isnan(nir[:,0])[mask])            
-            self.X = nir[mask][mask_]
-            self.Y = Y[mask][mask_]
-
+    def _process_data(self, data_raw, mask, spectrum_type, filter_keyword):
+        # Extract spectrum data
+        spectral_data = np.array(data_raw.filter(regex=spectrum_type).filter(regex=filter_keyword))
+        mask_ = ~(np.isnan(spectral_data[:, 0])[mask])
+        X = spectral_data[mask][mask_]
+        Y = np.array(data_raw.filter(regex="|".join(self.y_labels)))[mask][mask_]
+        return X , Y
 
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, index):
-        spectral_data = self.X[index, :-1]
+        spectral_data = self.X[index, :-1 ]
         label = self.Y[index]
         return spectral_data, np.log(label+1)
+    
+    def get_labels(self):
+        return(self.y_names)
+   
 
 
 # if __name__ == '__main__':
